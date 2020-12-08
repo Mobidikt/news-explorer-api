0718260814331d7ff3a25b2e61e3d56e9a3bfc63
Kirill Mecker <ghost3299@yandex.ru>
2020-12-08 20:43:29 +0400

    init: backend news api

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+const mongoDbUrl = 'mongodb://localhost:27017/newsdb';
+const mongoConnectOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+
+mongoose
+  .connect(mongoDbUrl, mongoConnectOptions)
+  .then(() => {
+    console.log('База данных подключена');
+  })
+  .catch((err) => {
+    console.log(`Ошибка при подключении базы данных: ${err}`);
+  });
+
+app.use(cors());
+
+// app.use(requestLogger);
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
+
+// app.post('/signin', validationUser, bodyParser.json(), login);
+// app.post('/signup', validationUser, bodyParser.json(), createUser);
+
+// app.use(routes);
+app.use(() => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+// app.use(errorLogger);
+// app.use(errors());
+app.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .send({ message: err.message || 'На сервере произошла ошибка' });
+  next();
+});
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
