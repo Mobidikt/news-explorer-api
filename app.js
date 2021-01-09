6591b2b16d109048b333fb5607b98ba8f97dbb02
Kirill Mecker <ghost3299@yandex.ru>
2021-01-09 19:35:36 +0400

    fix: mongo

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-require('dotenv').config();
-const express = require('express');
-const { errors } = require('celebrate');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const routes = require('./routes/index.js');
+require("dotenv").config();
+const express = require("express");
+const { errors } = require("celebrate");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes/index.js");
 
 const app = express();
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { createUser, login } = require('./controllers/controllersUsers.js');
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { createUser, login } = require("./controllers/controllersUsers.js");
 const {
   validationLogin,
   validateRegister,
-} = require('./middlewares/validation');
-const NotFoundError = require('./middlewares/errors/NotFoundError.js');
-const { NOT_FOUND, SERVER } = require('./libs/messages');
+} = require("./middlewares/validation");
+const NotFoundError = require("./middlewares/errors/NotFoundError.js");
+const { NOT_FOUND, SERVER } = require("./libs/messages");
 
-const { MONGO = 'mongodb://localhost:27017/newsdb', PORT = 3000 } = process.env;
+const { MONGO = "mongodb://localhost:27017/newsdb", PORT = 3000 } = process.env;
 
 const mongoConnectOptions = {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useUnifiedTopology: true,
   useFindAndModify: false,
 };
 
@@ -31,8 +32,8 @@ app.use(cors());
 app.use(requestLogger);
 
 app.use(bodyParser.json());
-app.post('/signin', validationLogin, login);
-app.post('/signup', validateRegister, createUser);
+app.post("/signin", validationLogin, login);
+app.post("/signup", validateRegister, createUser);
 app.use(routes);
 app.use(() => {
   throw new NotFoundError(NOT_FOUND);
